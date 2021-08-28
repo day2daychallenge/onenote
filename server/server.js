950d76d87e425fcf1c14e890a15a9567819aea34
Amir Hatami <bird_artus@yahoo.com>
2021-08-28 13:32:48 +0800

    add basic get, post, put and delete command

--- server/server.js
+++ server/server.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello from OneNote server!')
 })
 
+app.post('/', function (req, res) {
+    res.send('Got a POST request')
+})
+
+app.put('/note', function (req, res) {
+    res.send('Got a PUT request at /note')
+})
+
+app.delete('/note', function (req, res) {
+    res.send('Got a DELETE request at /note')
+})
+
 app.listen(port, () => {
   console.log(`app server listening at http://localhost:${port}`)
 })
